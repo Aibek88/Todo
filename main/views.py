5c6589d1c09aa81a49e49c7753cc7377d642c410
Aibek <aibek100288@mail.ru>
2021-01-30 23:28:24 +0600

    added urls

--- main/views.py
+++ main/views.py
@@ -4,4 +4,5 @@ def homepage(request):
     return HttpResponse("hello world")
 def test(request):
     return render(request, "test.html")
-
+def second(request):
+    return HttpResponse("test 2 page")
